cc751a3ab21bbd8cd094e0feb5d050e5b2dbdba3
Tushar <tusharfulzele920@gmail.com>
2024-10-05 13:12:13 +0530

    homepage is updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/Newmovies" element={<NewMovies />} />
           <Route path="/popular" element={<Popular />} />
           <Route path="/contact" element={<Contact />} />
